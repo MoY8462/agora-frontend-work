badf911775691ba7f8e65f5042cab5a49670fe82
MoY8462 <moy.sav21@hotmail.com>
2023-04-08 11:12:48 -0500

    style: :art: add font nunito

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">Hello World!</h1>
+  <h1 className="text-3xl font-bold underline nunito">Hello World!</h1>
 </template>
 
 <style>
